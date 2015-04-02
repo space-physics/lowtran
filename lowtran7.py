b001d96fd4db103e266f4cf94c83f28c8b2ff928
scienceopen <scienceopen@users.noreply.github.com>
2015-04-02 06:24:45 -0400

    intially working

--- lowtran7.py
+++ lowtran7.py
@@ -7,19 +7,36 @@ model circa 1994.
 """
 from __future__ import division,print_function,absolute_import
 from matplotlib.pyplot import figure,show
+from numpy import asarray,float32
 
-import lowtran
+import lowtran7 as lt7
 
-def golowtran(obsalt,zenang):
+def golowtran(obsalt,zenang,wlnm):
+    wlcminvstep = 500
+    wlnm= asarray(wlnm).astype(float32)
+    wlcminv = 1e7/wlnm
+    nwl = int((wlcminv[0]-wlcminv[1])/wlcminvstep)+1
 
-    lowtran.card3.h1 = obsalt
-    lowtran.card3.angle=zenang
 
+
+    #TX,V,ALAM,TRACE,UNIF,SUMA = lt7.lwtrn7(True,nwl)
+    TX,V,ALAM = lt7.lwtrn7(True,nwl,wlcminv[1],wlcminv[0],wlcminvstep,
+                           5,3,0,
+                           float32(obsalt),float32(0.),float32(zenang))[:3]
+    return TX[:,9],ALAM
 if __name__=='__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='Lowtran 7 interface')
-    p.add_argument('-z','--obsalt',help='altitude of observer [km]',type=float,default=0)
-    p.add_argument('-a','--zenang',help='zenith angle [deg]',type=float,default=0)
+    p.add_argument('-z','--obsalt',help='altitude of observer [km]',type=float,default=0.)
+    p.add_argument('-a','--zenang',help='zenith angle [deg]',type=float,default=0.)
+    p.add_argument('-w','--wavelen',help='wavelength range nm (start,stop)',type=float,nargs=2,default=(300,1200))
     p=p.parse_args()
 
-    golowtran(p.obsalt,p.zenang)
+    trans,wlnm = golowtran(p.obsalt,p.zenang,p.wavelen)
+
+    print(trans)
+
+    ax = figure().gca()
+    ax.plot(wlnm*1e3,trans)
+    ax.set_xlabel('wavelength [nm]')
+    show()
