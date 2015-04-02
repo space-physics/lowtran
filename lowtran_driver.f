d4b5ac0756b8d27f556278fbb02ca06b396bf692
scivision <scivision@users.noreply.github.com>
2015-04-02 06:24:45 -0400

    intially working

--- lowtran_driver.f
+++ lowtran_driver.f
@@ -4,41 +4,48 @@
 
 c       Python .true.:   Use common blocks (from f2py)
 c       Python .false.: Read the Tape5 file (like it's the 1960s again)
-        Logical, Parameter :: Python= .true. 
+        Logical, Parameter :: Python= .True. 
         
-        Integer, Parameter :: nwl = 201  ! number of wavelengths
+        Integer, Parameter :: nwl = 51  ! number of wavelengths
         Integer, Parameter :: ncol = 63  ! number of columns in output
         Real :: TXPy(nwl,ncol), VPy(nwl), ALAMPy(nwl), TRACEPy(nwl),
-     &      UNIFPy(nwl), SUMAPy(nwl)
+     &      UNIFPy(nwl), SUMAPy(nwl),H1,H2,ANGLE,V1,V2,DV
+        Integer MODEL,ITYPE,IEMSCT
 
-        call LWTRN7(Python,
-     &   nwl,TXPy,VPy,ALAMPy,TRACEPy,UNIFPy, SUMAPy)
+        V1=8333.; V2=33333.; DV=500.; MODEL=5; ITYPE=3;IEMSCT=0
+        H1=0.; H2=0.; ANGLE=0.
+
+        call LWTRN7(Python,nwl,V1,V2,DV,
+     &  TXPy,VPy,ALAMPy,TRACEPy,UNIFPy, SUMAPy,
+     &  MODEL,ITYPE,IEMSCT,
+     &  H1,H2,ANGLE)
 
         Write (*,'(A21/,201F7.0)') 'for wavelengths [nm]:', 1e3*ALAMPy
         Write (*,'(A21/,201F7.3)') 'transmission:',TXPy(1:nwl,9)
 
         End Program
         
-        Block Data setcards
-        Integer   MODEL,ITYPE,IEMSCT,M1,M2,M3,IM,NOPRT,M4,M5,M6,MDEF
-     &    IRD1,IRD2
-        Real      TBOUND,SALB,H1,H2,ANGLE,RANGE,BETA,RE
         
-        Common /CARD1/MODEL,ITYPE,IEMSCT,M1,M2,M3,IM,NOPRT,TBOUND,SALB
-        Data Model/5/, ITYPE/3/, IEMSCT/0/, M1/0/,M2/0/,M3/0/,NOPRT/0/,
-     &       TBOUND/0/,SALB/0/
-
-        COMMON /CARD1A/ M4,M5,M6,MDEF,        IRD1,IRD2
-        Data M4/0/,M5/0/,M6/0/,MDEF/0/ !No need to init IRD1,IRD2
-        
-        COMMON /CARD2/ IHAZE,ISEASN,IVULCN,ICSTL,ICLD,IVSA,VIS,WSS,WHH,
-     &    RAINRT
-        DATA IHAZE/0/  !no need for the rest
-        
-        COMMON /CARD3/ H1,H2,ANGLE,RANGE,BETA,RE,LEN
-        Data H1/0./,H2/0./,Angle/0./
-        
-        COMMON /CARD4/ V1,V2,DV
-        Data V1/10000./, V2/20000./, DV/50./
-        
-        End Block Data setcards
+c        Block Data setcards
+c        Integer   MODEL,ITYPE,IEMSCT,M1,M2,M3,IM,NOPRT,M4,M5,M6,MDEF
+c     &    IRD1,IRD2
+c        Real      TBOUND,SALB,H1,H2,ANGLE,RANGE,BETA,RE
+c        
+c        Common /CARD1/MODEL,ITYPE,IEMSCT,M1,M2,M3,IM,NOPRT,TBOUND,SALB
+c        Data Model/5/, ITYPE/3/, IEMSCT/0/, M1/0/,M2/0/,M3/0/,NOPRT/0/,
+c     &       TBOUND/0/,SALB/0/
+c
+c        COMMON /CARD1A/ M4,M5,M6,MDEF,        IRD1,IRD2
+c        Data M4/0/,M5/0/,M6/0/,MDEF/0/ !No need to init IRD1,IRD2
+c        
+c        COMMON /CARD2/ IHAZE,ISEASN,IVULCN,ICSTL,ICLD,IVSA,VIS,WSS,WHH,
+c     &    RAINRT
+c        DATA IHAZE/0/  !no need for the rest
+c        
+c        COMMON /CARD3/ H1,H2,ANGLE,RANGE,BETA,RE,LEN
+c        Data H1/0./,H2/0./,Angle/0./
+c        
+c       COMMON /CARD4/ V1,V2,DV
+c        Data V1/8333./, V2/33333./, DV/500./
+c        
+c        End Block Data setcards
