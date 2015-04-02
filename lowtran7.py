8c5734bf268ea424c9b7d0925097890b27d9bec8
scienceopen <scienceopen@users.noreply.github.com>
2015-04-02 07:04:22 -0400

    better plotting

--- lowtran7.py
+++ lowtran7.py
@@ -7,36 +7,57 @@ model circa 1994.
 """
 from __future__ import division,print_function,absolute_import
 from matplotlib.pyplot import figure,show
-from numpy import asarray,float32
+from matplotlib.colors import LogNorm
+from numpy import asarray,arange
 
 import lowtran7 as lt7
 
 def golowtran(obsalt,zenang,wlnm):
-    wlcminvstep = 500
-    wlnm= asarray(wlnm).astype(float32)
+    wlcminvstep = 20
+    wlnm= asarray(wlnm)
     wlcminv = 1e7/wlnm
     nwl = int((wlcminv[0]-wlcminv[1])/wlcminvstep)+1
 
 
 
     #TX,V,ALAM,TRACE,UNIF,SUMA = lt7.lwtrn7(True,nwl)
-    TX,V,ALAM = lt7.lwtrn7(True,nwl,wlcminv[1],wlcminv[0],wlcminvstep,
+    T = []
+    for za in zenang:
+        TX,V,ALAM = lt7.lwtrn7(True,nwl,wlcminv[1],wlcminv[0],wlcminvstep,
                            5,3,0,
-                           float32(obsalt),float32(0.),float32(zenang))[:3]
-    return TX[:,9],ALAM
+                          obsalt,0,za)[:3]
+        T.append(TX[:,9])
+    return T,ALAM*1e3
+
+def plottrans(wlnm,trans,log):
+    ax = figure().gca()
+    for za,t in zip(zenang,trans):
+        ax.plot(wlnm[1:],t[1:],label=str(za))
+    ax.set_xlabel('wavelength [nm]')
+    ax.set_ylabel('transmission (unitless)')
+    ax.set_title('zenith angle [deg] = '+str(zenang))
+    ax.legend(loc='best')
+    ax.grid(True)
+    if log:
+        ax.set_yscale('log')
+        ax.set_ylim(bottom=1e-5)
+    ax.invert_xaxis()
+    ax.set_xlim(left=wlnm[0])
+
 if __name__=='__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='Lowtran 7 interface')
     p.add_argument('-z','--obsalt',help='altitude of observer [km]',type=float,default=0.)
-    p.add_argument('-a','--zenang',help='zenith angle [deg]',type=float,default=0.)
-    p.add_argument('-w','--wavelen',help='wavelength range nm (start,stop)',type=float,nargs=2,default=(300,1200))
+    p.add_argument('-a','--zenang',help='zenith angle [deg] (start,stop,step)',type=float,nargs='+',default=(0,25+12.5,12.5))
+    p.add_argument('-w','--wavelen',help='wavelength range nm (start,stop)',type=float,nargs=2,default=(200,1500))
     p=p.parse_args()
 
-    trans,wlnm = golowtran(p.obsalt,p.zenang,p.wavelen)
+    zenang = arange(p.zenang[0],p.zenang[1],p.zenang[2])
 
-    print(trans)
+    trans,wlnm = golowtran(p.obsalt,zenang,p.wavelen)
 
-    ax = figure().gca()
-    ax.plot(wlnm*1e3,trans)
-    ax.set_xlabel('wavelength [nm]')
+    plottrans(wlnm,trans,False)
+    plottrans(wlnm,trans,True)
+
+
     show()
