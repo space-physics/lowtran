4e27ac4c864dfbb7feb78e527cda379a973f289f
scienceopen <scienceopen@users.noreply.github.com>
2015-04-09 14:19:48 -0400

    docs

--- pylowtran7.py
+++ pylowtran7.py
@@ -4,24 +4,49 @@ Michael Hirsch
 GPLv3+
 Python wrapper of the venerable LOWTRAN7 atmospheric absorption and solar transmission
 model circa 1994.
+For now, assumes arctic environment
+
+Note: specified Lowtran7 model limitations include
+wlcminvstep >= 20 cm^-1
+0 <= wlcminv <= 50000
+
+historical note:
+developed on CDC CYBER, currently runs on 32-bit single float--this can cause loss of numerical
+precision, future would like to ensure full LOWTRAN7 code can run at 64-bit double float.
+
+user manual:
+www.dtic.mil/dtic/tr/fulltext/u2/a206773.pdf
+
+Right now a lot of features are not implemented, please submit a request for more!
 """
 from __future__ import division,print_function,absolute_import
 from matplotlib.pyplot import figure,show
 from pandas import DataFrame
 from numpy import asarray,arange,atleast_1d
 from os import mkdir
+from warnings import warn
 
 import lowtran7 as lt7
 
-def golowtran(obsalt,zenang,wlnm):
-    zenang=atleast_1d(zenang)
+def golowtran(obsalt_km,zenang_deg,wlnm):
+    obsalt_km = atleast_1d(obsalt_km)
+    if obsalt_km.size>1:
+        obsalt_km = obsalt_km[0]
+        warn('** LOWTRAN7: for now I only handle single altitudes. Using first value of {} [km]'.format(obsalt_km))
+
+    zenang_deg=atleast_1d(zenang_deg)
+
     wlcminv,wlcminvstep,nwl =nm2lt7(wlnm)
+    if wlcminvstep<5:
+        warn('** LOWTRAN7: minimum resolution 5 cm^-1, specified resolution 20 cm^-1')
+    if not (0<=wlcminv) & (wlcminv<=50000):
+        warn('** LOWTRAN7: specified model range 0 <= wlcminv <= 50000')
     #TX,V,ALAM,TRACE,UNIF,SUMA = lt7.lwtrn7(True,nwl)
     T = []
-    for za in zenang:
+    for za in zenang_deg:
         TX,V,ALAM = lt7.lwtrn7(True,nwl,wlcminv[1],wlcminv[0],wlcminvstep,
                            5,3,0,
-                          obsalt,0,za)[:3]
+                          obsalt_km,0,za)[:3]
         T.append(TX[:,9])
     T = asarray(T).T
 
