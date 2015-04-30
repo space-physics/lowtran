31fb300e9c992eac64481e5122048d5fa4907cd3
scienceopen <scienceopen@users.noreply.github.com>
2015-04-29 22:45:24 -0400

    allowing more input variety

--- test.py
+++ test.py
@@ -2,7 +2,9 @@
 from pylowtran7 import golowtran
 from numpy.testing import assert_array_almost_equal
 
-T = golowtran(0,0,[500,500.5])
+c1={'model':5,'itype':3,'iemsct':0}
 
-assert_array_almost_equal([500.500488,500],T.index)
-assert_array_almost_equal([0.85731113, 0.85709256],T.values.squeeze())
+T = golowtran(0,0,[500,900],c1)
+
+assert_array_almost_equal([900.090027,  500.],T.index.values[[0,-1]])
+assert_array_almost_equal([0.87720001, 0.85709256],T.values[[0,-1]].squeeze())
