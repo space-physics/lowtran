15b79a1c01b7d3a5732534ccb146ee808b437e2d
scienceopen <scienceopen@users.noreply.github.com>
2015-06-11 18:38:00 -0400

    nose

--- test.py
+++ test.py
@@ -2,9 +2,11 @@
 from pylowtran7 import golowtran
 from numpy.testing import assert_array_almost_equal
 
-c1={'model':5,'itype':3,'iemsct':0}
+def test_atmosphere_transmission():
 
-T = golowtran(0,0,[500,900],c1)
+    c1={'model':5,'itype':3,'iemsct':0}
 
-assert_array_almost_equal([900.090027,  500.],T.index.values[[0,-1]])
-assert_array_almost_equal([0.87720001, 0.85709256],T.values[[0,-1]].squeeze())
+    T = golowtran(0,0,[500,900],c1)
+
+    assert_array_almost_equal([900.090027,  500.],T.index.values[[0,-1]])
+    assert_array_almost_equal([0.87720001, 0.85709256],T.values[[0,-1]].squeeze())
