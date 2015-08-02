4a1212b5a07fcc514232266ba6f3e653cf192fed
scienceopen <hirsch617@gmail.com>
2015-08-02 04:02:17 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from numpy.distutils.core import Extension
+
+with open('README.rst') as f:
+	long_description = f.read()
+	
+setup(name='lowtran',
+      version='0.1',
+	  description='Python wrapper for LOWTRAN7 atmosphere transmission model',
+	  long_description=long_description,
+	  author='Michael Hirsch',
+	  author_email='hirsch617@gmail.com',
+	  url='https://github.com/scienceopen/lowtran',
+	  install_requires=['numpy','six','nose','pytz','pandas'],
+      packages=['lowtran'],
+#      ext_modules=[Extension(name='lowtran7',sources=['lowtran7.f'],
+#                    f2py_options=['quiet'])]
+	  )
+  	  
