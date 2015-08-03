3270133a02c10cc07ffe6badab730a1967769ccc
scienceopen <hirsch617@gmail.com>
2015-08-02 22:42:16 -0400

    cleanup

--- setup.py
+++ setup.py
@@ -7,15 +7,6 @@ from numpy.distutils.core import setup,Extension
 with open('README.rst') as f:
 	long_description = f.read()
 
-#%% compile f2py fortran module
-#if len(sys.argv) < 3 or sys.argv[2] != '--uninstall':
-#    from subprocess import Popen
-#    sysCall = ['f2py', '--quiet', '-m', 'lowtran7', '-c', 'lowtran7.f']
-#    procout = Popen(sysCall, shell=False)
-#    sout,serr = procout.communicate() #timeout is incompatible with Python 2.7
-    #print(sout.decode('utf8')) # works for python 2 and 3
-    #note: serr always seems to be None even if real error
-
 #%% install
 setup(name='lowtran',
       version='0.1',
@@ -24,7 +15,7 @@ setup(name='lowtran',
 	  author='Michael Hirsch',
 	  author_email='hirsch617@gmail.com',
 	  url='https://github.com/scienceopen/lowtran',
-	  install_requires=['numpy','six','nose','pytz','pandas'],
+	  install_requires=['numpy','six','pytz','pandas'],
       packages=['lowtran'],
       ext_modules=[Extension(name='lowtran7',sources=['lowtran7.f'],
                     f2py_options=['quiet'])]
