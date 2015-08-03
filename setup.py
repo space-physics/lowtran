f2e535f87fe04cb54d4ea9913761880c63f15754
scivision <scivision@users.noreply.github.com>
2015-08-02 21:44:08 -0400

    better linkage

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
+import sys
+#from setuptools import setup
+from numpy.distutils.core import setup,Extension
 
-from setuptools import setup
-from numpy.distutils.core import Extension
 
 with open('README.rst') as f:
 	long_description = f.read()
-    
-#%% compile f2py fortran module    
-from subprocess import Popen,PIPE
-sysCall = ['f2py', '--quiet', '-m', 'lowtran7', '-c', 'lowtran7.f']
-procout = Popen(sysCall,stdout=PIPE, shell=False)
-sout,serr = procout.communicate() #timeout is incompatible with Python 2.7
-print(sout.decode('utf8')) # works for python 2 and 3
-#note: serr always seems to be None even if real error
+
+#%% compile f2py fortran module
+#if len(sys.argv) < 3 or sys.argv[2] != '--uninstall':
+#    from subprocess import Popen
+#    sysCall = ['f2py', '--quiet', '-m', 'lowtran7', '-c', 'lowtran7.f']
+#    procout = Popen(sysCall, shell=False)
+#    sout,serr = procout.communicate() #timeout is incompatible with Python 2.7
+    #print(sout.decode('utf8')) # works for python 2 and 3
+    #note: serr always seems to be None even if real error
 
 #%% install
 setup(name='lowtran',
@@ -24,7 +26,7 @@ setup(name='lowtran',
 	  url='https://github.com/scienceopen/lowtran',
 	  install_requires=['numpy','six','nose','pytz','pandas'],
       packages=['lowtran'],
-#      ext_modules=[Extension(name='lowtran7',sources=['lowtran7.f'],
-#                    f2py_options=['quiet'])]
+      ext_modules=[Extension(name='lowtran7',sources=['lowtran7.f'],
+                    f2py_options=['quiet'])]
 	  )
   	  
