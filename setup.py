7dfb471c9cdc4407d219f2f0d7603b8755679e2b
scienceopen <scienceopen@users.noreply.github.com>
2016-02-23 13:07:28 -0500

    setup

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(name='lowtran',
 	  )
 	 
 try:
-    subprocess.call(['conda','install','--yes','--quiet','--file','requirements.txt'],shell=False) #don't use os.environ
+    subprocess.run(['conda','install','--yes','--quiet','--file','requirements.txt'])
 except Exception as e:
     print('you will need to install packages in requirements.txt  {}'.format(e))
+    with open('requirements.txt','r') as f:
+        print(f.read())
