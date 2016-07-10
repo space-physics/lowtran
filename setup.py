3e64e331defff0aa0bc06e08cbc2cd31579836f9
scivision <scivision@users.noreply.github.com>
2016-07-10 00:14:09 -0400

    doc

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import os,sys
 import setuptools #enables develop
 import subprocess
 
-exepath = os.path.dirname(sys.executable)
 try:
-    subprocess.call([os.path.join(exepath,'conda'),'install','--yes','--file','requirements.txt'])
+    subprocess.call(['conda','install','--yes','--file','requirements.txt'],
+                    cwd=os.path.dirname(sys.executable))
 except Exception as e:
-    print('tried conda in {}, but you will need to install packages in requirements.txt  {}'.format(exepath,e))
-
-
-with open('README.rst','r') as f:
-	long_description = f.read()
+    pass
 
 #%% install
 from numpy.distutils.core import setup,Extension
 
 setup(name='lowtran',
 	  description='Python wrapper for LOWTRAN7 atmosphere transmission model',
-	  long_description=long_description,
 	  author='Michael Hirsch',
 	  url='https://github.com/scienceopen/lowtran',
       install_requires=['pathlib2'],
