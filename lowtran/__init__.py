2dee7ec557f7e3ea3a882686af8415ce6573a983
scienceopen <scienceopen@users.noreply.github.com>
2016-07-10 00:14:09 -0400

    doc

--- lowtran/__init__.py
+++ lowtran/__init__.py
@@ -1,13 +1,6 @@
-from __future__ import division,absolute_import,unicode_literals,print_function
-from six import PY2
-if PY2:
-    from pathlib2 import Path
-else:
+try:
     from pathlib import Path
-#%% apply future to each module
-flist = Path(__file__).parent.glob('*.py')
-flist = [f for f in flist if f!='__init__.py']
-__all__=flist
+    Path().expanduser()
+except (ImportError,AttributeError):
+    from pathlib2 import Path
 
-for f in flist:
-    from . import f #thereby applying __future__
