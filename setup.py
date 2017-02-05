9ac1367edf89e83093cb1c7b5b66e1fe697d28b1
scivision <scivision@users.noreply.github.com>
2017-02-05 03:03:59 -0500

    fallback

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ try:
     conda.cli.main('install','--file','requirements.txt')
 except Exception as e:
     print(e)
+    import pip
+    pip.main(['install','-r','requirements.txt'])
 
 #%% install
 from numpy.distutils.core import setup,Extension
