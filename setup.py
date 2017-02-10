a696bc1bb0070def729f122254b1f02e84d14b73
scienceopen <scienceopen@users.noreply.github.com>
2017-02-10 01:47:22 -0500

    pythonic-install

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 import setuptools #enables develop
-
-try:
-    import conda.cli
-    conda.cli.main('install','--file','requirements.txt')
-except Exception as e:
-    print(e)
-    import pip
-    pip.main(['install','-r','requirements.txt'])
-
 #%% install
 from numpy.distutils.core import setup,Extension
 
+req=['python-dateutil','pytz','nose','numpy','xarray','matplotlib','seaborn']
+
 setup(name='lowtran',
+        author='Michael Hirsch, Ph.D',
+
+        install_requires=req,
       ext_modules=[Extension(name='lowtran7',sources=['lowtran7.f'],
                     f2py_options=['--quiet'])]
 	  )
