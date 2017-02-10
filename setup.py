d4778eedac464bb32028f85085fedcc3686471ca
scivision <scivision@users.noreply.github.com>
2017-02-10 11:11:12 -0500

    prereq

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import setuptools #enables develop
+
+#%% Monkey patch necessary since setup_requires is not adequate
+import pip
+pip.main(['install','numpy'])
 #%% install
 from numpy.distutils.core import setup,Extension
 
 req=['python-dateutil','pytz','nose','numpy','xarray','matplotlib','seaborn']
 
 setup(name='lowtran',
-        author='Michael Hirsch, Ph.D',
-       setup_requires=['numpy'],
-        install_requires=req,
+      author='Michael Hirsch, Ph.D',
+      install_requires=req,
       ext_modules=[Extension(name='lowtran7',sources=['lowtran7.f'],
                     f2py_options=['--quiet'])]
 	  )
