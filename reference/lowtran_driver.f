5d819d9a704b4ba95d586cb65b3a31c0dea66351
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-15 10:37:33 -0400

    comments

--- reference/lowtran_driver.f
+++ reference/lowtran_driver.f
@@ -1,18 +1,47 @@
+! Comments by Michael Hirsch, Ph.D. https://www.scivision.co
+! p. = page, s. = section of Lowtran7 user manual
+! p. 19(28) s. 3.1 begins to describe the Card format
+! Remember that:
+!  * Fortran is case-insensitive:   XY = xy = Xy = xY
+!  * Fortran ignores spaces, punctuation is all that matters.
+!  * Fortran 77 can only have program text in columns 7..72
+!  * Fortran 77 comments must start in column 1 or >= 7
+!  * Fortran 77 columns 2-5 for line numbers, column 6 for continuation symbol
         Program LowtranDemo
 
-        Implicit none
+        implicit none
 
 !       Python .true.:   Use common blocks (from f2py)
 !       Python .false.: Read the Tape5 file (like it's the 1960s again)
-        Logical, Parameter :: Python= .true.
+        logical, parameter :: Python= .true.
 
-        Integer, Parameter :: nwl = 51  ! number of wavelengths
-        Integer, Parameter :: ncol = 63  ! number of columns in output
+! Model bounds and resolution (can't increase resolution beyond model limits)
+      integer, Parameter :: nwl = 51  ! number of wavelengths
+      integer, Parameter :: ncol = 63  ! number of columns in output
+      real, Parameter :: v1=8333., v2=33333. ! wavenumber cm^-1 bounds
+! Model configuration, see Lowtran manual p. 21(30) s. 3.2
+! Use only one of the following model scenarios
+
+!!! Auroral oval Model e.g. central Alaska !!!
+      integer, parameter :: model=5 ! 5: subarctic winter
+      integer, parameter :: itype=3 ! 3: vertical or slant path to space
+      integer, parameter :: iemsct=0! 0: transmittance model
+! in lowtran7.f, I set M1-M6, MDEF all =0 per p.21
+
+!!! Horizontal model (only way to use meterological data) !!!
+!      integer, parameter :: model=0 ! 0: Specify meterological data (horiz path)
+!      integer, parameter :: itype=1 ! 1: Horizontal, constant pressure path
+!      integer, parameter :: iemsct=0! 0: transmittance model
+! TODO M1-M6=0 to use JCHAR of card 2C.1 (p.22)
+
+!-------- END model config -----------------
+
+!-------- array assignments --------
         Real :: TXPy(nwl,ncol), VPy(nwl), ALAMPy(nwl), TRACEPy(nwl),
-     &      UNIFPy(nwl), SUMAPy(nwl),H1,H2,ANGLE,V1,V2,DV
-        Integer MODEL,ITYPE,IEMSCT
+     &      UNIFPy(nwl), SUMAPy(nwl),H1,H2,ANGLE,DV
 
-        V1=8333.; V2=33333.; DV=500.; MODEL=5; ITYPE=3;IEMSCT=0
+
+        DV=500.
         H1=0.; H2=0.; ANGLE=0.
 
         call LWTRN7(Python,nwl,V1,V2,DV,
@@ -25,13 +54,14 @@
 
         end program
 
-
-c        Block Data setcards
-c        Integer   MODEL,ITYPE,IEMSCT,M1,M2,M3,IM,NOPRT,M4,M5,M6,MDEF
-c     &    IRD1,IRD2
-c        Real      TBOUND,SALB,H1,H2,ANGLE,RANGE,BETA,RE
-c
-c        Common /CARD1/MODEL,ITYPE,IEMSCT,M1,M2,M3,IM,NOPRT,TBOUND,SALB
+! 
+!------------ obsolete ------------
+!        Block Data setcards
+!        Integer   MODEL,ITYPE,IEMSCT,M1,M2,M3,IM,NOPRT,M4,M5,M6,MDEF
+!     &    IRD1,IRD2
+!        Real      TBOUND,SALB,H1,H2,ANGLE,RANGE,BETA,RE
+!
+!        Common /CARD1/MODEL,ITYPE,IEMSCT,M1,M2,M3,IM,NOPRT,TBOUND,SALB
 c        Data Model/5/, ITYPE/3/, IEMSCT/0/, M1/0/,M2/0/,M3/0/,NOPRT/0/,
 c     &       TBOUND/0/,SALB/0/
 c
