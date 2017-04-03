d55eeb0552466625fa2670de02cdac3253283cb6
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-03 19:01:14 -0400

    label

--- SolarIrradiance.py
+++ SolarIrradiance.py
@@ -9,8 +9,8 @@ if __name__=='__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='Lowtran 7 interface')
     p.add_argument('-z','--obsalt',help='altitude of observer [km]',type=float,default=0.)
-    p.add_argument('-a','--zenang',help='zenith angle [deg]  can be single value or list of values',type=float,nargs='+',default=[0])
-    p.add_argument('-w','--wavelen',help='wavelength range nm (start,stop)',type=float,nargs=2,default=(200,2500))
+    p.add_argument('-a','--zenang',help='zenith angle [deg]  can be single value or list of values',type=float,default=0.)
+    p.add_argument('-w','--wavelen',help='wavelength range nm (start,stop)',type=float,nargs=2,default=(200,15000))
     p.add_argument('--model',help='0-6, see Card1 "model" reference. 5=subarctic winter',type=int,default=5)
     p=p.parse_args()
 
@@ -24,6 +24,6 @@ if __name__=='__main__':
  
     plottrans(trans,p.zenang,False)
 
-    plotirrad(irrad)
+    plotirrad(irrad,True)
 
     show()
