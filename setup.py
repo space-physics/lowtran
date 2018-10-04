e7b637b8d798dca1584ca2b955725085310d44a1
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-10-04 19:19:14 -0400

    windows auto-mingw [travis skip]

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 import setuptools  # noqa: F401
 from numpy.distutils.core import setup, Extension
+import os
+from pathlib import Path
+
+
+if os.name == 'nt':
+    sfn = Path(__file__).parent / 'setup.cfg'
+    stxt = sfn.read_text()
+    if '[build_ext]' not in stxt:
+        with sfn.open('a') as f:
+            f.write("[build_ext]\ncompiler = mingw32")
 
 ext = [Extension(name='lowtran7',
                  sources=['src/lowtran7.f'],
