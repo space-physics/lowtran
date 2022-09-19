62c8156e13a1292207fee9e2a90eaa7e9f43db4b
Michael Hirsch <scivision@users.noreply.github.com>
2022-09-18 22:20:43 -0400

    add codecheck to buildfile

--- buildfile.m
+++ buildfile.m
@@ -1,5 +1,13 @@
 function plan = buildfile
 plan = buildplan(localfunctions);
+plan.DefaultTasks = "test";
+plan("test").Dependencies = "check";
+end
+
+function checkTask(~)
+% Identify code issues (recursively all Matlab .m files)
+issues = codeIssues;
+assert(isempty(issues.Issues),formattedDisplayText(issues.Issues))
 end
 
 function testTask(~)
