d494866319e71b7558b8b2d4c4ef2164186a910d
scivision <scivision@users.noreply.github.com>
2023-02-10 16:52:07 -0500

    Windows: add needed flag for MinGW

--- src/lowtran/cmake/f2pyTarget.cmake
+++ src/lowtran/cmake/f2pyTarget.cmake
@@ -4,9 +4,14 @@ function(f2py_target module_name module_src out_dir)
 
 set(out ${CMAKE_CURRENT_BINARY_DIR}/${module_name}${f2py_suffix})
 
+set(f2py_arg --quiet -m ${module_name} -c ${module_src})
+if(WIN32 AND CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
+  list(APPEND f2py_arg --compiler=mingw32)
+endif()
+
 add_custom_command(
   OUTPUT ${out}
-  COMMAND ${f2py} --quiet -m ${module_name} -c ${module_src}
+  COMMAND ${f2py} ${f2py_arg}
   )
 
 add_custom_target(${module_name} ALL DEPENDS ${out})
