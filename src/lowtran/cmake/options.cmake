4e30020272b334d32708027630d06ecc6cd38c83
scivision <scivision@users.noreply.github.com>
2023-02-22 02:02:43 -0500

    better CMake .gitignore [skip ci]

--- src/lowtran/cmake/options.cmake
+++ src/lowtran/cmake/options.cmake
@@ -14,7 +14,4 @@ if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR} CACHE PATH "Install top-level directory" FORCE)
 endif()
 
-# --- auto-ignore build directory
-if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
-  file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")
-endif()
+file(GENERATE OUTPUT .gitignore CONTENT "*")
